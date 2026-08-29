846a2402ba7e4d6389d16899508f9c6c62b593d1
rouxli <312761023+rouxli@users.noreply.github.com>
2026-08-29 19:53:44 +0800

    8/29 约分最简分式+修正拼写

--- C/2026.8.29-求序列N项和.c
+++ C/2026.8.29-求序列N项和.c
@@ -3,18 +3,18 @@
 int main()
 {
     int n;
-    double divident,divisor;
+    double dividend, divisor;
     double sum = 0.0;
     int i;
     double t;
 
     scanf("%d", &n);
-    divident = 2;
+    dividend = 2;
     divisor = 1;
     for ( i = 1; i <= n; i++ ) {
-        sum += divident / divisor;
-        t = divident;
-        divident = divident + divisor;
+        sum += dividend / divisor;
+        t = dividend;
+        dividend = dividend + divisor;
         divisor = t;
     }
     printf("%.2f\n",sum);
